5a2550ff53ad6ab8397a084c34291dba89ba4a8d
Damir <denikolop@mail.ru>
2026-05-14 21:09:55 +0500

    Разработанный контекст данных для модели задач.

---
+++ Classes/TasksContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager_Khodzhiev.Classes.Database;
+
+namespace TaskManager_Khodzhiev.Classes
+{
+    class TasksContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.Connection, Config.Version);
+        }
+    }
+}
