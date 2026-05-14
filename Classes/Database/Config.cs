4c32bccc238c29ab612afdd5e8e9969c9b8ccf29
Damir <denikolop@mail.ru>
2026-05-14 20:44:36 +0500

    Созданный класс Config.

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Khodzhiev.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string Connection = "server=localhost;uid=root;password=;database=TaskManager;;";
+        public static readonly MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 32));
+    }
+}
