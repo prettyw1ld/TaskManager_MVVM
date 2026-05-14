a1b39d735c0e28c6f130ea7472ed04f2bfeca589
Damir <denikolop@mail.ru>
2026-05-14 20:35:17 +0500

    Разработанная главная страница и пользовательский элемент управления. Ходжиев

---
+++ View/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace TaskManager_Khodzhiev.View
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main(object Context)
+        {
+            InitializeComponent();
+            DataContext = Context;
+        }
+    }
+}
